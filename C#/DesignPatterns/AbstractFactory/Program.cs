bccf55fee12fb6e2cdb08c356523ed089126ec51
Michal Tichý <email@tichymichal.net>
2025-05-26 22:01:49 +0200

    migrated to .net 8

--- C#/DesignPatterns/AbstractFactory/Program.cs
+++ C#/DesignPatterns/AbstractFactory/Program.cs
@@ -4,9 +4,10 @@
 	{
 		static void Main(string[] args)
 		{
-			//All currently supported animals are from America, add support to specify other continents during AnimalsCreator initialization. Use abstract factory.
-			var animalCreator = new AnimalsCreator();
-			foreach (var animal in animalCreator.CreateAnimals().Take(10))
+            //All currently supported animals are from America, add support to specify other continents during AnimalsCreator initialization. Use abstract factory.
+            // Create multiple animal creators for different continents needed, they should share common interface. 
+            var animalCreator = new AnimalsCreator();
+            foreach (var animal in animalCreator.CreateAnimals().Take(10))
 			{
 				Console.WriteLine($"{animal.GetType().Name} - {animal.Food}");
 			}
